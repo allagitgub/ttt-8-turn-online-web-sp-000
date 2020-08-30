be1b0fb44e2812207fea808712fbaf238e3abf81
Alla Kuzminova <alchik@gmail.com>
2020-08-30 13:26:29 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+def display_board(board)
+  puts " #{board[0]} "+"|"+" #{board[1]} "+"|"" #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} "+"|"+" #{board[4]} "+"|"" #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} "+"|"+" #{board[7]} "+"|"" #{board[8]} "
+end
+
+def valid_move?(board, index)
+  return index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  return !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+def input_to_index(input)
+  index = input.to_i
+  index - 1
+end
+
+def move(board, index, value="X")
+  board[index] = value
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+end
